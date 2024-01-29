eebb0572bed7992fb10beed087246a10cc5c1b52
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-01-29 11:06:23 -0600

    feature: catch errors with rrd

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Local imports.
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import AppLayout from "./ui/AppLayout";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
+import Error from "./ui/Error";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Order from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
     {
         element: <AppLayout />,
+        errorElement: <Error />,
         children: [
             {
                 path: '/',
@@ -21,6 +23,7 @@ const router = createBrowserRouter([
                 path: '/menu',
                 element: <Menu />,
                 loader: menuLoader,
+                errorElement: <Error />,
             },
             {
                 path: '/cart',
