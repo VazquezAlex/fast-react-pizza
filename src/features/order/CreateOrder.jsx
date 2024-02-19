1c12d0a854e535e7e38901245cc86f8a2eacc237
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-02-19 12:52:39 -0600

    feature: added error handling when submitting a form

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,13 +1,13 @@
 // import { useState } from "react";
 
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
-// const isValidPhone = (str) =>
-//   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-//     str
-//   );
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
 
 const fakeCart = [
   {
@@ -36,9 +36,14 @@ const fakeCart = [
 function CreateOrder() {
 
     // const [withPriority, setWithPriority] = useState(false);
-    const cart = fakeCart;
-    console.log(cart);
 
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === "submitting";
+
+    const formErrors = useActionData();
+
+    const cart = fakeCart;
+    
     return (
         <div>
             <h2>Ready to order? Let&apos;s go!</h2>
@@ -54,6 +59,7 @@ function CreateOrder() {
                     <div>
                         <input type="tel" name="phone" required />
                     </div>
+                    { formErrors?.phone && <p>{ formErrors.phone }</p> }
                 </div>
 
                 <div>
@@ -80,7 +86,9 @@ function CreateOrder() {
                         name = "cart"
                         value = { JSON.stringify(cart) }
                     />
-                    <button>Order now</button>
+                    <button disabled = { isSubmitting }>
+                        { isSubmitting ? 'Placing Order' : 'Order now' }
+                    </button>
                 </div>
             </Form>
         </div>
@@ -97,6 +105,14 @@ export async function action({ request }) {
         priority: data.priority === "on",
     }
 
+    // Add errors if there is any.
+    const errors = {};
+    if (!isValidPhone(order.phone)) errors.phone = 'Please give us a correct phone number';
+
+    // If there is an error, we return the errors object.
+    if (Object.keys(errors).length > 0) return errors;
+    
+    // If no errors, we create the order and redirect.
     const newOrder = await createOrder(order);
 
     return redirect(`/order/${ newOrder.id }`);
